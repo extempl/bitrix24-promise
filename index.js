a09bdde6e9c08b0a1c3ab92530acdce2f0fd0bcc
Anton Konovalov <extempl@gmail.com>
2020-04-26 09:30:56 +0300

    Update to support direct url authorization

--- index.js
+++ index.js
@@ -119,6 +119,10 @@ exports.manualAuthURL = function(req, res){
     if (Object.keys(credentials).length === 0) throw new Error('Bitrix24 was not properly initialized before calling auth.');
     else res.redirect(credentials.auth.tokenHost+credentials.auth.authorizePath+'?scope='+scope+'&response_type=code&client_id='+credentials.client.id);
 };
+exports.manualAuthDirectURL = function(req, res){
+    if (Object.keys(credentials).length === 0) throw new Error('Bitrix24 was not properly initialized before calling auth.');
+    else res.redirect(`${bitrixUrl}${credentials.auth.authorizePath}?response_type=code&client_id=${credentials.client.id}`);
+};
 exports.authenticate = function()
 {
     return function(req, res, next){
